b02ffc95cf300fb7c7741a8769865cf5fb5a7643
Victor Castillo <victor.castillo@mvm.com.co>
2020-08-27 07:53:51 -0500

    docs agregado

---
+++ docs/precache-manifest.7d14f2856b56c186a771ff9890d8c28b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd57f5d27bf839b7bf4c229bc4ae37da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dff785f7b7550ce9db3",
+    "url": "/static/css/main.fe11baee.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dff785f7b7550ce9db3",
+    "url": "/static/js/main.fbd57398.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
